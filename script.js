c3a503359dc8f3925810d6da8f770d685005db8a
Yerassyl <kanapiyanovyerassyl@gmail.com>
2026-04-19 20:48:38 +0500

    functional without removing and changing read

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const addBookButton = document.getElementById("add-book-button");
 const cancelDialog = document.getElementById("cancel");
 const form = document.getElementById("dialog-form");
 const booksView = document.querySelector(".books");
-const sumbitButton = document.getElementById("#submit");
+const submitButton = document.getElementById("submit");
+
 
 
 
@@ -11,6 +12,7 @@ const sumbitButton = document.getElementById("#submit");
 
 addBookButton.addEventListener("click", function (){
     dialog.showModal();
+    
 })
 
 cancelDialog.addEventListener("click", function (){
@@ -19,8 +21,10 @@ cancelDialog.addEventListener("click", function (){
 
 
 const myLibrary = [];
+const created = [];
 
 function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -29,37 +33,69 @@ function Book(title, author, pages, read) {
 
 Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this);
+    updateBooks();
 };
 
-// submitButton.addEventListener("click", function (){
-//     sampleCards.forEach((result) => {
 
-//     const card = document.createElement("div");
-//     card.classList = "card-body";
 
+
+function updateBooks(){
+    myLibrary.forEach(function(element){
+        if (created.includes(element.id)){
+            //skip
+        }else{
+            createBookCard(element);
+            created.push(element.id);
+        }
+    })
+}
+
+function createBookCard(book) {
+    const div = document.createElement("div");
+    const pTitle = document.createElement("p");
+    pTitle.textContent = '"' + book.title + '"';
     
-//     const content = `
-//         <div class="card">
-//         <div class="card-header" id="coolCards">
-//         <img src="${result.image}" class="cardImage">
-        
-//         </div>
-    
-//             <div class="card-body">
-    
-//             <h5>${result.title}</h5>
-//             <p>${result.description}</p>
-        
-            
-            
-//             </div>
-    
-//         </div>
-//         `;
+    const pAuthor = document.createElement("p");
+    pAuthor.textContent = book.author;
 
-//     container.innerHTML += content;
-//     });
-// })
+    const pPages = document.createElement("p");
+    pPages.textContent = book.pages;
+
+    const buttonRead = document.createElement("button");
+    // if read then read if not ,not read
+    if(book.read === "on"){
+        buttonRead.textContent = "Read";
+    }else {
+        buttonRead.textContent = "Not Read";
+    }
+
+    const buttonRemove = document.createElement("button");
+    buttonRemove.textContent = "Remove";
 
 
+    div.classList.add("book")
+    buttonRead.classList.add("read-book");
+    buttonRemove.id = "remove-book";
 
+    div.appendChild(pTitle);
+    div.appendChild(pAuthor);
+    div.appendChild(pPages);
+    div.appendChild(buttonRead);
+    div.appendChild(buttonRemove);
+    booksView.appendChild(div);
+}
+
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const formData = new FormData(form);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    const read = formData.get("read");
+
+    const book = new Book(title, author, pages, read);
+    book.addBookToLibrary();
+    form.reset();
+    dialog.close();
+})
